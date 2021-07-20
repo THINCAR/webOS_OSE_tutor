2795655ad4ff40058da6cb124832c4e732629e0a
Seo HyeonMyeong <gusaud2199@naver.com>
2021-07-20 17:09:49 +0900

    Update : New method (getInfo, getProperties, setProperties, setFormat) there is a little change in css sytlesheet, I try and try but , startPreview, setFormat methods doesn't work.

--- webapp/index.js
+++ webapp/index.js
@@ -33,14 +33,13 @@ window.onload = function() {
     function getCameraList_callback(msg){
         var arg = JSON.parse(msg);
         if (arg.returnValue) {
-            var camList = arg.deviceList;
-            var listLabel = document.getElementById("label_cam_list");
+            var list = arg.deviceList;
+            var label = document.getElementById("label_cam_list");
 
-            if(camList!==""){
-                listLabel.innerHTML = "";
-                camList.forEach(element => {
-                    listLabel.innerHTML = listLabel.innerHTML + element.id;
-                    console.log(element.id);
+            if(list!==""){
+                label.innerText = "";
+                list.forEach(element => {
+                    label.innerText = label.innerText + element.id;
                 });
             }
             else{
@@ -56,6 +55,8 @@ window.onload = function() {
     function openCamera_callback(msg){
         var arg = JSON.parse(msg);
         if (arg.returnValue) {
+            var label = document.getElementById("label_cam_open");
+            label.innerHTML = "handle: " + arg.handle;
             console.log("[openCamera] Success [handle: " + arg.handle + "]");
         }
         else{
@@ -72,6 +73,72 @@ window.onload = function() {
             console.error("[openCamera] Failed, error <" + arg.errorCode + "> : " + arg.errorText);
         }
     }
+
+    function getInfo_callback(msg){
+        var arg = JSON.parse(msg);
+        if (arg.returnValue) {
+            var info = arg.info;
+            var label = document.getElementById("label_get_info");
+            if (info!=="") {
+                label.innerText = "";
+                for (var key in info){
+                    label.innerText = label.innerText + key + " : " + info[key] + "\n";
+                    console.log(key + " : " + info[key]);
+                }
+                console.log(info.details.video.maxWidth)
+            }
+            else{
+                label.innerText = "None Info";
+            }
+            console.log("[getInfo] Success");
+        }
+        else{
+            console.error("[getInfo] Failed, error <" + arg.errorCode + "> : " + arg.errorText);
+        }
+    }
+
+    function getProperties_callback(msg){
+        var arg = JSON.parse(msg);
+        if (arg.returnValue) {
+            var prop = arg.params;
+            var label = document.getElementById("label_get_prop");
+            if (prop!=="") {
+                label.innerText = "";
+                for (var key in prop){
+                    label.innerText = label.innerText + key + " : " + prop[key] + "\n";
+                    console.log(key + " : " + prop[key]);
+                }
+            }
+            else{
+                label.innerText = "None Properties";
+            }
+            console.log("[getProperties] Success");
+        }
+        else{
+            console.error("[getProperties] Failed, error <" + arg.errorCode + "> : " + arg.errorText);
+        }
+    }
+
+    function setProperties_callback(msg){
+        var arg = JSON.parse(msg);
+        if (arg.returnValue) {
+            console.log("[setProperties] Success");
+        }
+        else{
+            console.error("[setProperties] Failed, error <" + arg.errorCode + "> : " + arg.errorText);
+        }
+    }
+
+    function setFormat_callback(msg){
+        var arg = JSON.parse(msg);
+        if (arg.returnValue) {
+            console.log("[setFormat] Success");
+        }
+        else{
+            console.error("[setFormat] Failed, error <" + arg.errorCode + "> : " + arg.errorText);
+        }
+    }
+
 
     /*
      * LS2 API 메서드 함수화
@@ -109,21 +176,68 @@ window.onload = function() {
         bridge.call(url,params);
     }
 
-    // camera2/startPreview
+    // camera2/startPreview(handle)
     function startPreview(handle){
-        var url = 'luna://com.webos.service.camera2/startPreview'
+        var url = 'luna://com.webos.service.camera2/startPreview';
         var params = JSON.stringify({
-            "handle": handle,
+            "handle": Number(handle),
             "params":{
                 "type": "sharedmemory",
                 "source": "0"
             }
         })
-        console.log(params);
         bridge.onservicecallback = startPreview_callback;
         bridge.call(url,params);
     }
 
+    // camera2/getInfo(id)
+    function getInfo(id){
+        var url = 'luna://com.webos.service.camera2/getInfo';
+        var params = JSON.stringify({
+            "id": String(id)
+        })
+        bridge.onservicecallback = getInfo_callback;
+        bridge.call(url,params);
+    }
+
+    // camera2/getProperties(handle)
+    function getProperties(handle){
+        var url = 'luna://com.webos.service.camera2/getProperties';
+        var params = JSON.stringify({
+            "handle": Number(handle)
+        })
+        bridge.onservicecallback = getProperties_callback;
+        bridge.call(url,params);
+    }
+
+    // camera2/setProperties(handle)
+    function setProperties(handle){
+        var url = 'luna://com.webos.service.camera2/setProperties';
+        var params = JSON.stringify({
+            "handle": Number(handle),
+            "params": {
+
+            }
+        })
+        bridge.onservicecallback = setProperties_callback;
+        bridge.call(url,params);
+    }
+
+    // camera2/setFormat(handle)
+    function setFormat(handle){
+        var url = 'luna://com.webos.service.camera2/setFormat';
+        var params = JSON.stringify({
+            "handle": Number(handle),
+            "params": {
+                "width":640,
+                "height":480,
+                "format":"YUV",
+                "fps":30
+            }
+        })
+        bridge.onservicecallback = setFormat_callback;
+        bridge.call(url,params);
+    }
 
     /*
      * 이벤트 리스너 설정
@@ -138,14 +252,33 @@ window.onload = function() {
     }
 
     document.getElementById("button_cam_open").onclick = function() {
-        var id = document.getElementById("cam_id_input").value;
+        var id = document.getElementById("input_cam_open").value;
         openCamera(id);
     }
 
     document.getElementById("button_start_preview").onclick = function() {
-        var id = document.getElementById("cam_handle_input").value;
-        id = Number(id);
-        startPreview(id);
+        var handle = document.getElementById("input_start_preview").value;
+        startPreview(handle);
+    }
+
+    document.getElementById("button_get_info").onclick = function() {
+        var id = document.getElementById("input_get_info").value;
+        getInfo(id);
+    }
+
+    document.getElementById("button_get_prop").onclick = function() {
+        var handle = document.getElementById("input_get_prop").value;
+        getProperties(handle);
+    }
+
+    document.getElementById("button_set_prop").onclick = function() {
+        var handle = document.getElementById("input_set_prop").value;
+        setProperties(handle);
+    }
+
+    document.getElementById("button_set_format").onclick = function() {
+        var handle = document.getElementById("input_set_format").value;
+        setFormat(handle);
     }
 
     /*
