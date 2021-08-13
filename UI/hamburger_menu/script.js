c885a2bf31b86811cd5c1ac764c67f53efc6016e
Seo HyeonMyeong <gusaud2199@naver.com>
2021-08-13 15:12:29 +0900

    Delete: UI project

--- UI/hamburger_menu/script.js
+++
@@ -1,8 +0,0 @@
-let icons = document.querySelectorAll(".icon");
-
-for (var i = 0; i< icons.length; i++){
-    icons[i].addEventListener("click",(e)=>{
-        let target = e.target;
-        target.classList.toggle("active");
-    })
-}
